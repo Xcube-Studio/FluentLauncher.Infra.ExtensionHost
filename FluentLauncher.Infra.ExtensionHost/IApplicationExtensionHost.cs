ad2d4398db9a2c5c0bc14270a1c0971684711d05
natsurainko <a-275@qq.com>
2025-04-11 10:21:53 +0800

    从 Fluent Launcher 项目中分包管理

---
+++ FluentLauncher.Infra.ExtensionHost/IApplicationExtensionHost.cs
@@ -0,0 +1,17 @@
+﻿using FluentLauncher.Infra.ExtensionHost.Assemblies;
+using Microsoft.UI.Xaml.Markup;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace FluentLauncher.Infra.ExtensionHost;
+
+public interface IApplicationExtensionHost
+{
+    void Initialize(object application);
+
+    IExtensionAssembly GetExtensionAssembly(string pathToAssembl);
+
+    IDisposable RegisterXamlTypeMetadataProvider(IXamlMetadataProvider provider);
+
+    Uri LocateResource(object component, [CallerFilePath] string callerFilePath = "");
+}
